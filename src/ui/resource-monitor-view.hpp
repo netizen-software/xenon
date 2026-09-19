1ce1ee72e9a36d165b95c78a7c5e5829a935483c
tobynetizen <tobias.hand.0@protonmail.com>
2026-09-18 20:39:36 -0500

    feat: add resource usage graphs

--- src/ui/resource-monitor-view.hpp
+++ src/ui/resource-monitor-view.hpp
@@ -2,6 +2,8 @@
 
 #include <gtk/gtk.h>
 
+#include <deque>
+
 #include "core/resources/resource-monitor.hpp"
 
 namespace xenon {
@@ -17,13 +19,24 @@ class ResourceMonitorView {
     GtkWidget* getWidget() const;
 
    private:
+    static void onCpuGraphDraw(GtkDrawingArea* area, cairo_t* context, int width, int height,
+                               gpointer userData);
+    static void onMemoryGraphDraw(GtkDrawingArea* area, cairo_t* context, int width, int height,
+                                  gpointer userData);
     static gboolean onRefresh(gpointer userData);
+    void addSample(std::deque<double>& history, double value);
+    void drawGraph(cairo_t* context, int width, int height, const std::deque<double>& history,
+                   double red, double green, double blue) const;
     void refresh();
 
     GtkWidget* widget_;
     GtkLabel* cpuValue_;
     GtkLabel* memoryValue_;
+    GtkDrawingArea* cpuGraph_;
+    GtkDrawingArea* memoryGraph_;
     guint refreshSourceId_;
+    std::deque<double> cpuHistory_;
+    std::deque<double> memoryHistory_;
     ResourceMonitor resourceMonitor_;
 };
 
